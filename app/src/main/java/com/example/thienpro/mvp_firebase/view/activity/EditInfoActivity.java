520eba0ee38a692ba141b5cc5262c6907d61855a
ThienPro <vanthien113@gmail.com>
2017-11-11 23:38:19 +0700

    fix edit profile

--- app/src/main/java/com/example/thienpro/mvp_firebase/view/activity/EditInfoActivity.java
+++ app/src/main/java/com/example/thienpro/mvp_firebase/view/activity/EditInfoActivity.java
@@ -4,11 +4,14 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.thienpro.mvp_firebase.R;
 import com.example.thienpro.mvp_firebase.databinding.ActivityEditinfoBinding;
 import com.example.thienpro.mvp_firebase.model.entity.User;
 import com.example.thienpro.mvp_firebase.presenter.RegisterPresenter;
+import com.example.thienpro.mvp_firebase.presenter.impl.RegisterPresenterImpl;
 import com.example.thienpro.mvp_firebase.view.EditInfoView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -18,6 +21,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Map;
+
 /**
  * Created by ThienPro on 11/10/2017.
  */
@@ -26,23 +31,36 @@ public class EditInfoActivity extends AppCompatActivity implements EditInfoView 
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
     private ActivityEditinfoBinding binding;
-    private User user;
     private RegisterPresenter registerPresenter;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_editinfo);
+
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
         binding.setEvent(this);
+        registerPresenter = new RegisterPresenterImpl(mDatabase);
+        user = mAuth.getCurrentUser();
 
-
-        ValueEventListener postListener = new ValueEventListener() {
+        ValueEventListener valueEventListener = new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // Get Post object and use the values to update the UI
-                User post = dataSnapshot.getValue(User.class);
+
+                @SuppressWarnings("unchecked")
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                String firstValue = (String) map.get("address");
+                String secondValue = (String) map.get("email");
+                String thirdValue = (String) map.get("name");
+                Boolean fourValue = (Boolean) map.get("sex");
+
+                if (fourValue == false)
+                    binding.rbEditnu.setChecked(true);
+
+                User user = new User(secondValue, thirdValue, firstValue, fourValue);
                 binding.setData(user);
             }
 
@@ -52,13 +70,23 @@ public class EditInfoActivity extends AppCompatActivity implements EditInfoView 
                 // ...
             }
         };
+
+        mDatabase.child("users").child(user.getUid()).addValueEventListener(valueEventListener);
     }
 
     @Override
     public void onSaveClick() {
-        FirebaseUser user = mAuth.getCurrentUser();
-        registerPresenter.writeNewUser(user.getUid().toString(), binding.etEditun.getText().toString(),
-                binding.etEditname.getText().toString(), binding.etEditadd.getText().toString(),
-                binding.rbEditnam.isChecked());
+        Log.e("THIEN", user.getUid());
+        Log.e("THIEN", binding.etEditun.getText().toString());
+        Log.e("THIEN", binding.etEditname.getText().toString());
+        Log.e("THIEN", binding.etEditadd.getText().toString());
+        try {
+            registerPresenter.updateUser(user.getUid().toString(), binding.etEditun.getText().toString(),
+                    binding.etEditname.getText().toString(), binding.etEditadd.getText().toString(),
+                    binding.rbEditnam.isChecked());
+            Toast.makeText(this, "Lưu thành công!", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Lưu không thành công!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
